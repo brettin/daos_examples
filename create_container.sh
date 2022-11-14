a09ecacf3faa2d48600ad0e99acb9ae55784ff64
Thomas Brettin <brettint@gmail.com>
2022-11-14 11:04:38 -0600

    Update create_container.sh

--- create_container.sh
+++ create_container.sh
@@ -1,16 +1,23 @@
-# create a container named Brettin0 on pool0
+# SC22 tutorial
+# https://github.com/NGIOproject/PMTutorial
+
+# Create a container named Brettin0 on pool0
 daos cont create -t POSIX -l Brettin0 pool0
 
-# mount container Brettin0 on /tmp/test
+# Creating a pool would be something an administrator does
+# daos cont create my_pool --label=my_container --type=POSIX
+
+# Mount container Brettin0 on /tmp/test
 dfuse -m /tmp/test --pool=pool0 --container=Brettin0
 
 # Enable interception library
+# Good for checkpointing, for example. Not for small byte I/O
 export LD_PRELOAD=/usr/lib64/libioil.so
 
-# do I/O on /tmp/test 
+# Do I/O on /tmp/test 
 
-# unmount the container
+# Unmount the container
 fusermount3 -u /tmp/test
 
-# delete the container
+# Delete the container
 daos cont destroy pool0 Brettin0
